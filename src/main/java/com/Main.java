1c8c8d3e4098fd15524ac20e2e91c76ae78520cf
liyu <403995403@qq.com>
2020-12-20 02:24:17 +0800

    提交主文件和辅助工具

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,34 @@
+package com;
+
+import com.a99f.pack.ApkOperation;
+
+/**
+ * Copyright (C) 2020 A99F.COM Inc. All rights reserved.
+ * This is source code from a99f-channel-apk-obfuscator.
+ * The distribution of any copyright must be permitted by QiaoWeiRen Company.
+ * 此代码归A99F(A99F.com)版权所有.
+ * 概要说明:
+ * 设计UI文档地址：
+ * 产品文档地址：
+ * 关联API地址 ：
+ * 讨论文档地址：
+ * 需求说明
+ * 安全性说明：
+ * 功能性说明：
+ * 性能要求；
+ * 输入参数：
+ * 输出参数：
+ * 数据库操作说明：
+ * 日期: Created by liyu on 2:15 上午.
+ * 作者: A99F
+ * 更新版本          日期            作者             备注
+ * v0001            2020/12/20     A99F            完成文件创建
+ * 规划TODO-LIST：
+ * 清单编号          预计日期         作者             状态               备注
+ * td0001           0000/00/00      A99F          实现/未实现/进行中
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApkOperation.doAPKOperation();
+    }
+}
